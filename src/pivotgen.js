bc25ddccdc6615b305ca907e525fb92326afb9ac
Richie Rump <jorriss@gmail.com>
2015-05-26 14:38:57 -0400

    Changed JS to Revealing Prototype Pattern. When AGG radio button clicked the dropdown moves to where the radio was clicked.

--- src/pivotgen.js
+++ src/pivotgen.js
@@ -1,190 +1,194 @@
-"use strict";
 
-        function parseQuery() {
-            var cols = splitSelectColumns(getSelectColumns(replaceEndLine(document.getElementById("querytext").value.trim()))),
-                queryColumns = [];
-                
-            for (var i = 0; i < cols.length; ++i) {
-                queryColumns.push(new columnInfo(getColumnName(cols[i]), false, false));
-            }
-            clearChildNode(document.getElementById('columnTable'));
-            document.getElementById('columnTable').innerHTML = getColumnTable(queryColumns);
-        }
+var PivotGen = function () {
+    "use strict";
+    this.queryColumns = [];
+};
 
-        function generateQuery(queryCols){
-            var agg = document.getElementsByName('aggRadio');
-            var pvt = document.getElementsByName('pivotRadio');
-            var disp = document.getElementsByName('displayCheckbox');
+PivotGen.prototype = (function () {
+    "use strict";
 
-            for (var i = 0, length = queryCols.length; i < length; i++) {
-                if (agg[i].checked) {
-                    queryCols[i].agg = true;
-                }
-                if (pvt[i].checked) {
-                    queryCols[i].pivot = true;
-                }
-                if (disp[i].checked) {
-                    queryCols[i].display = true;
-                }
-            }
-            document.getElementById("pivottext").value = getPivotQuery(queryCols ,document.getElementById("valuestext").value, document.getElementById("querytext").value, document.getElementById("aggselect").value);
-        }
+    var replaceEndLine = function (query) {
+        return query.replace(/\n/g, " ");
+    },
 
+    getSelectColumns = function (query) {
+        var exp = /SELECT\s*(.*)\s*FROM/im,
+            match = exp.exec(query);
+        return match[1];
+    },
 
-function clearChildNode(node){
-    while (node.hasChildNodes()) {
-        node.removeChild(node.firstChild);
-    }
-}
+    splitSelectColumns = function (columns) {
+        return columns.split(',');
+    },
 
-function replaceEndLine(query) {    
-    return query.replace(/\n/g, " ")
-}
-
-function getSelectColumns(query) {
-    var exp = /SELECT\s*(.*)\s*FROM/im;
-    var match = exp.exec(query);
-    return match[1];
-}
-
-function splitSelectColumns(columns) {
-    return columns.split(',');
-}
-
-function getColumnName(column) {
-    var re = /s*(?:(?:(?:(?:.*)\s*AS\s*\[?(\w*)\]?)\s*?,?)|\[?(?:\w*\.)?(\w*[^\]])\]?\s?,?)+\s*/im; 
-    var m;
-     var val = "";
+    getColumnName = function (column) {
+        var re = /s*(?:(?:(?:(?:.*)\s*AS\s*\[?(\w*)\]?)\s*?,?)|\[?(?:\w*\.)?(\w*[^\]])\]?\s?,?)+\s*/im, 
+            m,
+            val = "";
      
-    if (column !== null) {
-        if ((m = re.exec(column.trim())) !== null) {
-            if (m.index === re.lastIndex) {
-                re.lastIndex++;
+        if (column !== null) {
+            m = re.exec(column.trim());
+            if (m !== null) {
+                if (m.index === re.lastIndex) {
+                    re.lastIndex = re.lastIndex + 1;
+                }
             }
         }
-    }
     
-    if (m[1] == null) {
-        val = m[2];
-    }
-    else {
-        val = m[1];
-    }
-    return val;
-}
-
-function columnInfo(name, pivot, display, agg){
-    this.name = name;
-    this.pivot = pivot;
-    this.display = display;
-    this.agg = agg;
-}
-
-function getAggRadioInputGroup(columnName, checked){
-    var group = "";
-    
-    if (checked === true) {     
-    group += '<div class="input-group">';
-    group += '<span class="input-group-addon">';
-    group += '<input type="radio" name="aggRadio" value="' + columnName + '" checked>';
-    group += '</span>';
-    group += '<select id="aggselect" class="form-control input-sm">';
-    group += '<option value="avg">AVG</option>';
-    group += '<option value="checksum_agg">CHECKSUM_AGG</option>';
-    group += '<option value="count">COUNT</option>';
-    group += '<option value="count_big">COUNT_BIG</option>';
-    group += '<option value="max">MAX</option>';
-    group += '<option value="min">MIN</option>';
-    group += '<option value="sum">SUM</option>';
-    group += '<option value="stdev">STDEV</option>';
-    group += '<option value="stdevp">STDEVP</option>';
-    group += '<option value="var">VAR</option>';
-    group += '<option value="varp">VARP</option>';
-    group += '</select>';
-    } else {
-        group += '<input type="radio" name="aggRadio" class="radio-table" value="' + columnName + '">';
-    }
-    group += '</div><!-- /input-group -->';
-    
-    return group;
-}
-
-function getColumnTable(columnNames) {
-    var table = "<table class=\"table table-striped\">";
-    table += "<thead><tr><th>Column Name</th><th>Agg</th><th>Pivot</th><th>Display</th></th></thead><tbody>";
-    
-    for (var len = columnNames.length, i=0; i < len; ++i) {
-        table += "<tr>";
-        table += '<td class="col-md-4">' + columnNames[i].name + '</td>';
-        table += '<td class="col-md-4">' + getAggRadioInputGroup(columnNames[i].name, (i === 0 ? true : false));
-        table +=  "</td>";
-        table += "<td><input type=\"radio\" name=\"pivotRadio\" value=\"" + columnNames[i].name + "\"";
-        if (i === 1) { table += " checked"; }
-        table +=  "></td>";
-        table += "<td><input type=\"checkbox\" name=\"displayCheckbox\" value=\"" + columnNames[i].name + "\"></td>";
-        table += "</tr>";
-    }    
-    table += "</tbody></table>";
-    return table;
-}
-
-function getPivotValues(pivotValues) {
-    var pivotValueArr = pivotValues.split('\n');
-    var values = "";
-    
-    for (var len = pivotValueArr.length, i=0; i < len; ++i) {
-        if (i > 0) { values += ", "; }
-        values += "[" + pivotValueArr[i].trim() + "]";
-    }
-    return values;    
-}
-
-function getPivotColPivot(columns) {
-    var name = "";
-    
-    for (var len = columns.length, i=0; i < len; ++i) {
-        if (columns[i].pivot === true) {
-            name = columns[i].name;
-            break;
+        if (m[1] === undefined) {
+            val = m[2];
         }
-    }
-    return name;
-}
-
-function getPivotColAgg(columns) {
-    var name = "";
-    
-    for (var len = columns.length, i=0; i < len; ++i) {
-        if (columns[i].agg === true) {
-            name = columns[i].name;
-            break;
+        else {
+            val = m[1];
         }
-    }
-    return name;
-}
+        return val;
+    },
 
-function getPivotColDisplay(columns) {
-    var display = "";
-    
-    for (var len = columns.length, i=0; i < len; ++i) {
-        if (columns[i].display === true) {
-            display += columns[i].name + ", ";
+    ColumnInfo = function (name, pivot, display, agg) {
+        this.name = name;
+        this.pivot = pivot;
+        this.display = display;
+        this.agg = agg;
+    },
+
+    clearChildNode = function(node) {
+        while (node.hasChildNodes()) {
+            node.removeChild(node.firstChild);
         }
-    }
-    return display;
-}
+    },
 
-function getPivotQuery(columns, pivotValues, query, aggType) {
-    var values = getPivotValues(pivotValues);
-    var pivot = "";
-    pivot += "SELECT " +  getPivotColDisplay(columns) + values +"\n";
-    pivot += "FROM (\n" ;
-    pivot += query + "\n" ;
-    pivot += ") AS sourceTable\n";
-    pivot += "PIVOT (\n";
-    pivot += "  " + aggType.toUpperCase() + "(sourceTable." + getPivotColAgg(columns) + ")\n";
-    pivot += "  FOR sourceTable." + getPivotColPivot(columns) + " IN (" + values +")\n";
-    pivot += ") AS pivotTable;\n";
+    getAggRadioInputGroup = function (columnName, checked) {
+        var group = "";
 
-    return pivot;
-}
+        if (checked === true) {
+            group += '<div class="input-group" id="aggSelectDiv">';
+            group += '<span class="input-group-addon">';
+            group += '<input type="radio" name="aggRadio" id="aggSelectRadio" value="' + columnName + '" checked>';
+            group += '</span>';
+            group += '<select id="aggselect" class="form-control input-sm">';
+            group += '<option value="avg">AVG</option>';
+            group += '<option value="checksum_agg">CHECKSUM_AGG</option>';
+            group += '<option value="count">COUNT</option>';
+            group += '<option value="count_big">COUNT_BIG</option>';
+            group += '<option value="max">MAX</option>';
+            group += '<option value="min">MIN</option>';
+            group += '<option value="sum">SUM</option>';
+            group += '<option value="stdev">STDEV</option>';
+            group += '<option value="stdevp">STDEVP</option>';
+            group += '<option value="var">VAR</option>';
+            group += '<option value="varp">VARP</option>';
+            group += '</select>';
+            group += '</div><!-- /input-group -->';
+        } else {
+            group += '<input type="radio" name="aggRadio" class="radio-table" onclick="selectAgg(this)" value="' + columnName + '">';
+        }
+
+        return group;
+    },
+
+    getColumnTable = function (columnNames) {
+        var table ,
+            i = 0;
+            
+        table = "<table class=\"table table-striped\">";
+        table += "<thead><tr><th>Column Name</th><th>Agg</th><th>Pivot</th><th>Display</th></th></thead><tbody>";
+
+        for (i = 0; i < columnNames.length; i += 1) {
+            table += "<tr>";
+            table += '<td class="col-md-4">' + columnNames[i].name + '</td>';
+            table += '<td class="col-md-4">' + getAggRadioInputGroup(columnNames[i].name, (i === 0 ? true : false));
+            table += "</td>";
+            table += "<td><input type=\"radio\" name=\"pivotRadio\" value=\"" + columnNames[i].name + "\"";
+            if (i === 1) {
+                table += " checked";
+            }
+            table += "></td>";
+            table += "<td><input type=\"checkbox\" name=\"displayCheckbox\" value=\"" + columnNames[i].name + "\"></td>";
+            table += "</tr>";
+        }
+        table += "</tbody></table>";
+        return table;
+    },
+
+    getPivotValues = function (pivotValues) {
+        var pivotValueArr = pivotValues.split('\n'),
+            values = "",
+            i = 0;
+
+        for (i = 0; i < pivotValueArr.length; i += 1) {
+            if (i > 0) {
+                values += ", ";
+            }
+            values += "[" + pivotValueArr[i].trim() + "]";
+        }
+        return values;
+    },
+
+    getPivotColPivot = function (columns) {
+        var name = "",
+            i = 0;
+
+        for (i = 0; i < columns.length; i += 1) {
+            if (columns[i].pivot === true) {
+                name = columns[i].name;
+                break;
+            }
+        }
+        return name;
+    },
+    getPivotColAgg = function (columns) {
+        var name = "",
+            i = 0;
+
+        for (i = 0; i < columns.length; i += 1) {
+            if (columns[i].agg === true) {
+                name = columns[i].name;
+                break;
+            }
+        }
+        return name;
+    },
+    getPivotColDisplay = function (columns) {
+        var display = "",
+            i = 0;
+
+        for (i = 0; i < columns.length; i += 1) {
+            if (columns[i].display === true) {
+                display += columns[i].name + ", ";
+            }
+        }
+        return display;
+    },
+    
+    parseQuery = function (queryText, element) {
+        var cols = splitSelectColumns(getSelectColumns(replaceEndLine(queryText.trim()))),
+            i = 0;
+
+        this.queryColumns = [];
+        for (i = 0; i < cols.length; i += 1) {
+            this.queryColumns.push(new ColumnInfo(getColumnName(cols[i]), false, false));
+        }
+        clearChildNode(document.getElementById(element));
+        document.getElementById(element).innerHTML =  getColumnTable(this.queryColumns);
+    },
+
+    getPivotQuery = function (columns, pivotValues, query, aggType) {
+        var values = getPivotValues(pivotValues),
+            pivot = "";
+            
+        pivot += "SELECT " + getPivotColDisplay(columns) + values + "\n";
+        pivot += "FROM (\n";
+        pivot += query + "\n";
+        pivot += ") AS sourceTable\n";
+        pivot += "PIVOT (\n";
+        pivot += "  " + aggType.toUpperCase() + "(sourceTable." + getPivotColAgg(columns) + ")\n";
+        pivot += "  FOR sourceTable." + getPivotColPivot(columns) + " IN (" + values + ")\n";
+        pivot += ") AS pivotTable;\n";
+
+        return pivot;
+    };
+    return {
+        parseQuery: parseQuery,
+        pivotQuery: getPivotQuery
+    };
+}());
